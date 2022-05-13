89f71cba6e847eeaa4e11c2e104cb46dc0722d4c
AlienCoder <37622754+mrclcyn@users.noreply.github.com>
2022-05-13 13:56:56 +0200

    minor debug

--- controllers/userMiddlewares.js
+++ controllers/userMiddlewares.js
@@ -6,20 +6,25 @@ const verifyAdmin = (req, res, next) => {
   try {
     const verified = jwt.verify(token, process.env.SECRET);
     if (token !== process.env.TOKEN) throw err;
+
     req.user = verified;
+
     next();
   } catch (err) {
     res.status(400).send("Invalid token");
   }
 };
 
-const verifyUser = (req, res, next) => {
-  const token = req.header("authentication-token");
+const verifyUser = async (req, res, next) => {
+  const token = await req.header("authentication-token");
   if (!token) return res.status(401).send("Access denied");
 
   try {
     const user = jwt.verify(token, process.env.SECRET);
+    if (user.id !== req.params.id) throw err;
+
     req.user = user;
+
     next();
   } catch (err) {
     res.status(400).send("Invalid token");
